43c8230f5a09249509ee18264357d123cc1156e7
Justin Horn <justin.horn@tu-bs.de>
2019-10-31 15:51:57 +0100

    while-loop

--- test.c
+++ test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+#define CURRENTWORLD_INDEX 0
+
 int main() {
-	int c = 0;
-	printf("Hello %d world",c);
+
+	printf("Hello world \t %d \n",CURRENTWORLD_INDEX);
+	//string a = scanf("In which world are we?");
+	//printf("%s",a);
+	int c;
+
+	while((c = getchar()) != 'P') {
+		putchar(c);
+
+	}
 }
