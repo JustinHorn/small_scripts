fcb8ed2c44efe4bbaf77d7be82b51e33f8d0ae29
Justin Horn <justin.horn@tu-bs.de>
2019-11-09 19:34:45 +0100

    update

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 # Please write your make-skript here
 SOURCE = TTT
-all: TTT
+all: TTT doTTT
 
 TTT: 
 	clang -o $(SOURCE) -std=c11 -pedantic -Wall -Wextra $(SOURCE).c
 doTTT: TTT
-	./test
+	./TTT
 
 test: 
 	clang -o $(SOURCE) -std=c11 -pedantic -Wall -Wextra $(SOURCE).c
--- TTT.c
+++ TTT.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define FALSE 0
 #define TRUE 1
@@ -11,9 +12,12 @@
 #define TIE		-2
 #define VICTORY_PLAYER_ONE PLAYER_ONE
 #define VICTORY_PLAYER_TWO PLAYER_TWO
+#define WELCOME 	"Welcome to my TicTacToe. If you wann quit it just write altGr + q =@ into the console \n"
 
-
+void setUp();
+void play();
 void generateField(int field[]);
+int want_to_change_player_symbols();
 void setChar_of_Player(int player);
 char read_from_console_skip_new_line();
 int game_status(int field[]);
@@ -28,34 +32,25 @@ void announceEnd();
 
 int field[FIELD_SIZE];
 char char_field[FIELD_SIZE];
-char playerChars[2];
+char playerChars[2] = {'X','O'};
 int currentPlayer = PLAYER_ONE;
 
 
 int main() {
-	generateField(field);
-	setChar_of_Player(PLAYER_ONE);
-	setChar_of_Player(PLAYER_TWO);
-	while(game_status(field) == GAME_RUNNING) {
-		showField(field);
-		int move = getMove(currentPlayer);
-		if(is_move_legal(field,move) == TRUE) {
-			doMove(move, currentPlayer);
-		}
-		if(game_status(field) == GAME_RUNNING) {
-			currentPlayer = getNextPlayer();
-			showField(field);
-			int move = getMove(currentPlayer);
-			if(is_move_legal(field,move) == TRUE) {
-				doMove(move, currentPlayer);
-			}
-		}
-		currentPlayer = getNextPlayer();
-	}
+	setUp();
+	play();
+	showField(field);
 	announceEnd();
 }
 
-
+void setUp() {
+	printf(WELCOME);
+	generateField(field);
+	if(want_to_change_player_symbols() == TRUE) {
+		setChar_of_Player(PLAYER_ONE);
+		setChar_of_Player(PLAYER_TWO);
+	}
+}
 
 void generateField(int field[]) {
 	for(int i = 0; i < FIELD_SIZE;i++) {
@@ -63,37 +58,63 @@ void generateField(int field[]) {
 	}
 }
 
+int want_to_change_player_symbols() {
+	printf("Do you want to use costume Symbols instead of X and O?(y)\n");
+	if(read_from_console_skip_new_line() == 'y') {
+		return TRUE;
+	}
+	return FALSE;
+}
+
 void setChar_of_Player(int player) {
-	printf("Enter char of Player %d: \n",player);
+	printf("Enter char of Player %d whitespace will be %c: \n",player,playerChars[player]);
 	char c = read_from_console_skip_new_line();
-	playerChars[player] = c;
+	if(c != ' ') {
+		playerChars[player] = c;
+	}
 }
 
 char read_from_console_skip_new_line() {
 	char c;
 	while((c = getchar() ) == '\n') {}
+	if(c == '@'){exit(0);}
 	return c;
+}
+
+void play() {
+	while(game_status(field) == GAME_RUNNING) {
+		showField(field);
+		int move = getMove(currentPlayer);
+		if(is_move_legal(field,move) == TRUE) {
+			doMove(move, currentPlayer);
+			currentPlayer = getNextPlayer();
+		}
+	}
 }
 
 
 int game_status(int field[]) {
 	for(int i = 0; i < 3;i++) { // horizontal
-		if(field[i] != -1 && int_elements_equal(field[i],field[i+1],field[i+2]) == TRUE) {
+		if(field[i*3] != EMPTY_FIELD && int_elements_equal(field[i*3],field[i*3+1],field[i*3+2]) == TRUE) {
+			printf("horizontal %d",i);
 			return field[i];
 		}
 	}
 
 	for(int i = 0; i < 3;i++) { // vertical
-		if(field[i] != -1 && int_elements_equal(field[i],field[i+3],field[i+6]) == TRUE) {
+		if(field[i] != EMPTY_FIELD && int_elements_equal(field[i],field[i+3],field[i+6]) == TRUE) {
+			printf("vertical %d",i);
 			return field[i];
 		}
 	}
 
 
-	if(field[4] != -1 && int_elements_equal(field[0],field[4],field[8]) == TRUE) {
+	if(field[4] != EMPTY_FIELD && int_elements_equal(field[0],field[4],field[8]) == TRUE) {
+			printf("diagonal l->r");
 		return field[4];
 	}
-	if(field[4] != -1 && int_elements_equal(field[2],field[4],field[6]) == TRUE) {
+	if(field[4] != EMPTY_FIELD && int_elements_equal(field[2],field[4],field[6]) == TRUE) {
+			printf("diagonal r->l");
 		return field[4];
 	}
 
@@ -134,7 +155,7 @@ void update_char_field(int field[]) {
 }
 
 int getMove(int currentPlayer) {
-	printf("Its your turn Player %d or %c ! Make a move(0-8): \n",currentPlayer, playerChars[currentPlayer]);
+	printf("Its your turn Player (%d,%c) ! Make a move(0-8): \n",currentPlayer, playerChars[currentPlayer]);
 		
 	
 	return read_from_console_skip_new_line()-48;
@@ -166,6 +187,6 @@ void announceEnd() {
 	if(x == TIE) {
 		printf("It is a tie!\n");
 	} else {
-		printf("Congretulations Player %d // %c you won! \n",x,playerChars[x]);
+		printf("Congretulations Player (%d,%c) you won! \n",x,playerChars[x]);
 	}
 }
